a57a7d4440d34dec8bb58b7038776e89e42d8ecd
yalingL <yxl4427@g.rit.edu>
2019-01-20 22:23:28 -0500

    js project

---
+++ javascript/assets/data.js
@@ -0,0 +1,22 @@
+var theChoice = 'FirstChoice';
+var data = new Array();
+
+// first select option
+data[theChoice] = ['Pick A Drink','-- Select -- ','Hot','Cold'];
+//second select option
+data['Hot'] = ['Choose A kind','-- Select --','Coffee','Chocolate','Fruit Drink'];
+//Third select option
+data['Coffee'] = ['Select One Option','-- Select --','Macchiato','Latte','Mocha'];
+//Third select option
+data['Chocolate'] = ['Select One Option','-- Select --','Hot Chocolate','Caramel Chocolate','Hot Cocoa-Nut'];
+//Third select option
+data['Fruit Drink'] = ['Select One Option','-- Select --','Pumpkin Spice','Steamed Apple Juice','Tea and Cider'];
+//second select option
+data['Cold'] = ['Choose A Kind','-- Select --','Milkshake','Tea','Sode'];
+//Third select option
+data['Milkshake'] = ['Select One Option','-- Select --','Vanilla','Strawberry','Caramel'];
+//Third select option
+data['Tea'] = ['Select One Option','-- Select --','Black Tea','Green Tea','Milk Tea'];
+//Third select option
+data['Sode'] = ['Select One Option','-- Select --','Pepsi','Sprite','Fanta'];
+
